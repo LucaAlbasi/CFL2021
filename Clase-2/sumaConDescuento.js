436b73d0bf2f08198ad1bbd8a841d8dcd7589a4d
LucaAlbasi <Elluqaalbasi@gmail.com>
2021-04-23 20:54:23 -0300

    Clase-2

---
+++ Clase-2/sumaConDescuento.js
@@ -0,0 +1,8 @@
+let readlineSync = require("readline-sync");
+let precioProducto = 450.50;
+console.log("Este es el precio inicial;", precioProducto);
+let porcentajeDescuento = 0.1;
+let descuento = (precioProducto * variable);
+console.log("Este es el descuento:", descuento);
+let precioFinal = (precioProducto - descuento);
+console.log("Este es el precio final:", precioFinal)
